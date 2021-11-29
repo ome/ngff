16944e8e9324136764ca3b27389a0e116c8703ed
William Moore <w.moore@dundee.ac.uk>
2021-11-29 14:37:20 +0000

    Handle validation by image.schema or collection.schema
    Examples are still v0.3 but can be updated to v0.4

--- 0.4/tests/test_validation.py
+++ 0.4/tests/test_validation.py
@@ -1,45 +1,38 @@
-import os
 import json
 import glob
 
 import pytest
 
-from jsonschema import validate as js_valid
-from jsonschema.exceptions import ValidationError as JSErr
-
-
-def test_json(method, testfile):
-
-    if "invalid" in testfile:
-        with pytest.raises(JSErr):
-            method(testfile)
-    else:
-        method(testfile)
+from jsonschema import validate
+from jsonschema.exceptions import ValidationError
 
 
 def files():
-    return list(glob.glob(f"examples/valid/*.json")) + \
-        list(glob.glob(f"examples/invalid/*.json"))
+    return list(glob.glob(f"examples/*/valid/*.json")) + \
+        list(glob.glob(f"examples/*/invalid/*.json"))
 
 def ids():
     return [str(x).split("/")[-1][0:-5] for x in files()]
 
 
-@pytest.fixture(params=files(), ids=ids())
-def testfile(request):
-    return request.param
+@pytest.mark.parametrize("testfile", files(), ids=ids())
+def test_json(testfile):
+
+    if "invalid" in testfile:
+        with pytest.raises(ValidationError):
+            json_schema(testfile)
+    else:
+        json_schema(testfile)
 
 
-@pytest.fixture(params=("jsonschema",))
-def method(request):
-
-    with open('schemas/json_schema/collection.schema') as f:
+def json_schema(path):
+    # To validate e.g. /examples/collection/valid/*.json the
+    # schema_name will be 'collection'...
+    schema_name = path.replace("examples/", "").split("/")[0]
+    # ...so we can load the correct schema
+    with open(f'schemas/json_schema/{schema_name}.schema') as f:
         schema = json.loads(f.read())
 
-    def json_schema(path):
-        with open(path) as f:
-            test_json = json.loads(f.read())
-        return js_valid(instance=test_json, schema=schema)
-
-    return json_schema
-
+    with open(path) as f:
+        test_json = json.loads(f.read())
+    validate(instance=test_json, schema=schema)
