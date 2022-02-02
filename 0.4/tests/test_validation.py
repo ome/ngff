9602cb44aec68cb464870aee0918d8620614aa0f
Sebastien Besson <seb.besson@gmail.com>
2022-02-02 16:51:04 +0000

    Refactor test_validation to use RefResolver and validate against both schemas

--- 0.4/tests/test_validation.py
+++ 0.4/tests/test_validation.py
@@ -3,39 +3,53 @@ import glob
 
 import pytest
 
-from jsonschema import validate
+from jsonschema import RefResolver, Draft202012Validator
 from jsonschema.exceptions import ValidationError
 
 
-def files():
-    return list(glob.glob(f"examples/valid/*.json")) + \
-        list(glob.glob(f"examples/valid_if_not_strict/*.json")) + \
-        list(glob.glob(f"examples/invalid/*.json"))
+with open('schemas/image.schema') as f:
+    image_schema = json.load(f)
+with open('schemas/strict_image.schema') as f:
+    strict_image_schema = json.load(f)
+schema_store = {
+    image_schema['$id']: image_schema,
+    strict_image_schema['$id']: strict_image_schema,
+}
 
-def ids():
-    return [str(x).split("/")[-1][0:-5] for x in files()]
+resolver = RefResolver.from_schema(image_schema, store=schema_store)
+validator = Draft202012Validator(image_schema, resolver=resolver)
+strict_validator = Draft202012Validator(strict_image_schema, resolver=resolver)
+
+valid_files = list(glob.glob("examples/valid/*.json"))
+invalid_files = list(glob.glob("examples/invalid/*.json"))
+valid_not_strict_files = list(glob.glob("examples/valid_if_not_strict/*.json"))
 
 
-@pytest.mark.parametrize("testfile", files(), ids=ids())
-def test_json(testfile):
+def ids(files):
+    return [str(x).split("/")[-1][0:-5] for x in files]
 
-    if "examples/invalid/" in testfile:
+
+@pytest.mark.parametrize("testfile", valid_files, ids=ids(valid_files))
+def test_valid(testfile):
+    with open(testfile) as f:
+        json_file = json.load(f)
+        validator.validate(json_file)
+        strict_validator.validate(json_file)
+
+
+@pytest.mark.parametrize(
+    "testfile", valid_not_strict_files, ids=ids(valid_not_strict_files))
+def test_valid_not_strict_files(testfile):
+    with open(testfile) as f:
+        json_file = json.load(f)
+        validator.validate(json_file)
         with pytest.raises(ValidationError):
-            json_schema(testfile)
-    else:
-        json_schema(testfile)
+            strict_validator.validate(json_file)
 
 
-def json_schema(path):
-    # Load the correct schema
-    with open(path) as f:
-        test_json = json.loads(f.read())
-        # we don't have @type in this version
-        if "multiscales" in test_json:
-            schema_name = "image.schema"
-        else:
-            raise Exception("No schema found")
-
-    with open('schemas/' + schema_name) as f:
-        schema = json.loads(f.read())
-    validate(instance=test_json, schema=schema)
+@pytest.mark.parametrize("testfile", invalid_files, ids=ids(invalid_files))
+def test_invalid(testfile):
+    with open(testfile) as f:
+        json_file = json.load(f)
+        with pytest.raises(ValidationError):
+            validator.validate(json_file)
