6403d883aa1ca559cc0b37368b5f78a84245e22f
jmoore <josh@glencoesoftware.com>
2022-04-21 12:12:11 +0200

    Add schema & test for bf2raw metadata

--- latest/tests/test_validation.py
+++ latest/tests/test_validation.py
@@ -7,11 +7,15 @@ from jsonschema import RefResolver, Draft202012Validator
 from jsonschema.exceptions import ValidationError
 
 
+with open('schemas/bf2raw.schema') as f:
+    bf2raw_schema = json.load(f)
 with open('schemas/image.schema') as f:
     image_schema = json.load(f)
 with open('schemas/strict_image.schema') as f:
     strict_image_schema = json.load(f)
+
 schema_store = {
+    bf2raw_schema['$id']: bf2raw_schema,
     image_schema['$id']: image_schema,
     strict_image_schema['$id']: strict_image_schema,
 }
@@ -19,7 +23,9 @@ schema_store = {
 resolver = RefResolver.from_schema(image_schema, store=schema_store)
 validator = Draft202012Validator(image_schema, resolver=resolver)
 strict_validator = Draft202012Validator(strict_image_schema, resolver=resolver)
+bf2raw_validator = Draft202012Validator(bf2raw_schema, resolver=resolver)
 
+bf2raw_files = list(glob.glob("examples/bf2raw/*.json"))
 valid_strict_files = list(glob.glob("examples/valid_strict/*.json"))
 valid_files = list(glob.glob("examples/valid/*.json"))
 invalid_files = list(glob.glob("examples/invalid/*.json"))
@@ -29,6 +35,15 @@ invalid_but_dont_fail_files = list(
 
 def ids(files):
     return [str(x).split("/")[-1][0:-5] for x in files]
+
+
+@pytest.mark.parametrize(
+    "testfile", bf2raw_files, ids=ids(bf2raw_files))
+def test_bf2raw(testfile):
+    with open(testfile) as f:
+        data = ''.join(line for line in f if not line.lstrip().startswith('//'))
+        jsondata = json.loads(data)
+        bf2raw_validator.validate(jsondata)
 
 
 @pytest.mark.parametrize(
