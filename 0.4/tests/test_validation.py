522699ff49f3112e5b67a6b596ea96552fbf052f
Sebastien Besson <seb.besson@gmail.com>
2022-05-09 21:06:32 +0100

    Annotate JSON files used for JSON schema tests
    Rather than splitting files into individual folders, this commit makes
    use of top-level keys (valid, recommended) to indicate whether a file
    should validate against image.schema and strict_image.schema
    
    The unit tests are updated to use a single glob and detect the
    appropriate schema based on the presence of the multiscales key

--- 0.4/tests/test_validation.py
+++ 0.4/tests/test_validation.py
@@ -17,57 +17,44 @@ schema_store = {
 }
 
 resolver = RefResolver.from_schema(image_schema, store=schema_store)
-validator = Draft202012Validator(image_schema, resolver=resolver)
-strict_validator = Draft202012Validator(strict_image_schema, resolver=resolver)
 
-valid_strict_files = list(glob.glob("examples/valid_strict/*.json"))
-valid_files = list(glob.glob("examples/valid/*.json"))
-invalid_files = list(glob.glob("examples/invalid/*.json"))
-invalid_but_dont_fail_files = list(
-    glob.glob("examples/invalid_but_dont_fail/*.json"))
+# Dictionary of validators
+validators = {}
+strict_validators = {}
+validators["multiscales"] = Draft202012Validator(
+    image_schema, resolver=resolver)
+strict_validators["multiscales"] = Draft202012Validator(
+    strict_image_schema, resolver=resolver)
+
+
+json_files = list(glob.glob("examples/**/*.json"))
 
 
 def ids(files):
     return [str(x).split("/")[-1][0:-5] for x in files]
 
+def get_spec(jsondata):
+    for s in ["multiscales"]:
+        if s in jsondata.keys():
+            return s
+    raise Exception("No specification found %s" % jsondata.keys())
 
-@pytest.mark.parametrize(
-    "testfile", valid_strict_files, ids=ids(valid_strict_files))
-def test_valid_strict(testfile):
-    with open(testfile) as f:
+
+@pytest.mark.parametrize("json_file", json_files, ids=ids(json_files))
+def test_compliance(json_file):
+    with open(json_file) as f:
         data = ''.join(line for line in f if not line.lstrip().startswith('//'))
         jsondata = json.loads(data)
-        validator.validate(jsondata)
-        strict_validator.validate(jsondata)
 
-
-@pytest.mark.parametrize("testfile", valid_files, ids=ids(valid_files))
-def test_valid_files(testfile):
-    with open(testfile) as f:
-        json_file = json.load(f)
-        validator.validate(json_file)
+    spec = get_spec(jsondata)
+    if jsondata.get("valid", True):
+        validators[spec].validate(jsondata)
+    else:
         with pytest.raises(ValidationError):
-            strict_validator.validate(json_file)
+            validators[spec].validate(jsondata)
 
-
-@pytest.mark.parametrize("testfile", invalid_files, ids=ids(invalid_files))
-def test_invalid(testfile):
-    with open(testfile) as f:
-        json_file = json.load(f)
+    if jsondata.get("recommended", True) and jsondata.get("valid", True):
+        strict_validators[spec].validate(jsondata)
+    else:
         with pytest.raises(ValidationError):
-            validator.validate(json_file)
-        with pytest.raises(ValidationError):
-            strict_validator.validate(json_file)
-
-
-@pytest.mark.xfail
-@pytest.mark.parametrize(
-    "testfile", invalid_but_dont_fail_files,
-    ids=ids(invalid_but_dont_fail_files))
-def test_invalid_but_dontfail(testfile):
-    with open(testfile) as f:
-        json_file = json.load(f)
-        with pytest.raises(ValidationError):
-            validator.validate(json_file)
-        with pytest.raises(ValidationError):
-            strict_validator.validate(json_file)
+            strict_validators[spec].validate(jsondata)
