3270186087a304986ff65ebd84c2fe41e85d4662
Josh Moore <josh@openmicroscopy.org>
2023-03-14 09:04:45 +0100

    Move to markdown

--- conf.py
+++ conf.py
@@ -13,7 +13,8 @@ author = 'NGFF Community'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = ["myst_parser"]
+source_suffix = [".rst", ".md"]
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
