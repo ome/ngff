e183b9ac6c9d6337ec354f445778eaf7f7490646
Norman Rzepka <code@normanrz.com>
2024-11-21 11:48:00 +0100

    bump ubuntu

--- conf.py
+++ conf.py
@@ -7,7 +7,7 @@
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = 'NGFF'
-copyright = '2023, NGFF Community'
+copyright = '2024, NGFF Community'
 author = 'NGFF Community'
 
 # -- General configuration ---------------------------------------------------
