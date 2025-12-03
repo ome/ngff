a39f1fe4dd22e5cee7200187a46dea32fc811708
Johannes Soltwedel <38459088+jo-mueller@users.noreply.github.com>
2025-12-03 12:48:13 +0100

    Subproject ngff-spec under specfications directory
    - update build tools to handle ngff-spec contents
    - Make sure schemas are provided as html artefacts
    - deprecate bikeshed

--- conf.py
+++ conf.py
@@ -30,6 +30,9 @@ exclude_patterns = [
     "README.md",
     "LICENSE.md",
     "CONTRIBUTING.md",
+    "**/README.md",
+    "**/LICENSE.md",
+    "**/CONTRIBUTING.md",
 ]
 
 redirects = {
@@ -56,7 +59,7 @@ html_js_files = [
 ]
 
 html_extra_path = [
-    "_bikeshed",
+    "_html_extra",
 ]
 
 # ####################################
@@ -164,42 +167,49 @@ def post_process():
     import os
     import shutil
     import subprocess
+    import sys
 
-    versions = ["latest"] + glob.glob("[0-9]*")
-    for version in versions:
+    # build ngff-spec docs
+    ngff_spec_versions = [
+        {"submodule": "specifications/0.5", "target": "0.5"},
+        {"submodule": "specifications/0.6.dev2", "target": "0.6.dev2"},
+    ]
 
-        # Run bikeshed
-        index_file = f"{version}/index.bs"
-        output_file = index_file.replace("bs", "html")
-        output_dir = os.path.dirname(output_file)
-        target_dir = os.path.join("_bikeshed", output_dir)
+    serve_directory = "_html_extra"
+    shutil.rmtree(serve_directory, ignore_errors=True)
+    os.makedirs(serve_directory, exist_ok=True)
 
-        run_bikeshed = True
+    for spec_version in ngff_spec_versions:
+        submodule_dir = spec_version["submodule"]
+        target_dir = spec_version["target"]
 
-        # Give the loop a chance to skip files if no build is needed/requested
-        if "BIKESHED" not in os.environ and os.path.exists(output_file):
-            src_time = os.path.getmtime(index_file)
-            out_time = os.path.getmtime(output_file)
-            if src_time < out_time:
-                print(f"{index_file} unchanged")
-                run_bikeshed = False
+        if os.path.exists(submodule_dir):
+            print(f"Building ngff-spec docs for version {target_dir}...")
 
-        if run_bikeshed:
-            subprocess.check_call(
-                f"bikeshed spec {index_file} {output_file}", shell=True,
-            )
+            os.chdir(submodule_dir)
 
-        if os.path.exists(target_dir):
-            shutil.rmtree(target_dir)
-        shutil.copytree(output_dir, target_dir)
+            # prebuild ngff-spec examples and schemas
+            subprocess.check_call([sys.executable, "ngff_spec/pre_build.py"])
+            os.chdir("ngff_spec")
 
-        # Run json-schema-for-humans
-        try:
-            d = os.getcwd()
-            os.chdir("_bikeshed")
-            gen_version(version)
-        finally:
-            os.chdir(d)
+            # Set BASE_URL from environment (for GitHub Pages) or fallback
+            os.environ["§BASE_URL"] = f"/{target_dir}/"
+            subprocess.check_call(["jupyter", "book", "build", "--ci", "--html"])
+            os.chdir("../../..")
+
+            # copy built ngff-spec to _html_extra for serving
+            source = Path(submodule_dir) / "ngff_spec" / "_build" / "html"
+            target = Path(serve_directory) / target_dir
+            shutil.copytree(source, target)
+
+            # copy examples and schemas to bikeshed output
+            schema_files = glob.glob(os.path.join(
+                submodule_dir, "ngff_spec/schemas", '*.schema'), recursive=True)
+            target = Path(serve_directory) / target_dir / "schemas"
+            os.makedirs(target, exist_ok=True)
+
+            for schema_file in schema_files:
+                shutil.copy2(schema_file, target)
 
 
 post_process()
--- specifications/index.md
+++ specifications/index.md
@@ -3,13 +3,12 @@ Specifications
 
 OME-Zarr files have standardized metadata (that is the OME portion of "OME-Zarr") - each new version of OME-Zarr files has its own specification. Those specifications are listed below.
 
-The current released version of the OME-Zarr specification is <a href="../latest/index.html">0.5</a>.
+The current released version of the OME-Zarr specification is [0.5](0.5/ngff_spec/specification.md).
 
-<ul>
-  <li> <a href="../latest/index.html">latest</a> Alias for the latest version below</li>
-  <li> <a href="../0.5/index.html">0.5</a> November 2024</li>
-  <li> <a href="../0.4/index.html">0.4</a> February 2022</li>
-  <li> <a href="../0.3/index.html">0.3</a> August 2021</li>
-  <li> <a href="../0.2/index.html">0.2</a> March 2021</li>
-  <li> <a href="../0.1/index.html">0.1</a> November 2020</li>
-</ul>
+```{toctree}
+:maxdepth: 1
+:caption: available specifications
+
+0.5/ngff_spec/index.md
+0.6.dev2/ngff_spec/index.md
+```
