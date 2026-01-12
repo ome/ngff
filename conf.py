b8f036fe6c03aacef568cf5da8f1ee1691cad2ae
Johannes Soltwedel <38459088+jo-mueller@users.noreply.github.com>
2026-01-12 18:09:07 +0100

    use bibtex references and parsing on page

--- conf.py
+++ conf.py
@@ -13,7 +13,11 @@ author = "NGFF Community"
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ["myst_parser","sphinx_reredirects"]
+extensions = [
+    "myst_parser",
+    "sphinx_reredirects",
+    "sphinxcontrib.bibtex"]
+bibtex_bibfiles = ["references.bib"]
 source_suffix = [".rst", ".md"]
 myst_heading_anchors = 5
 myst_enable_extensions = ["deflist", "strikethrough"]
