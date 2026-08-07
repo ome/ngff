01f2cdd155643b37db9790789e4eeea3622e4e09
Tiago Lubiana <tiago.lubiana.alves@usp.br>
2026-08-07 10:57:55 -0300

    try to disable injected linkpreview from cards

--- _static/main.js
+++ _static/main.js
@@ -1,3 +1,10 @@
 $(document).ready( function () {
     $('table.datatable').DataTable();
 } );
+
+document.addEventListener("readthedocs-addons-data-ready", function () {
+  document.querySelectorAll("a.link-preview, .sd-card a.sd-stretched-link").forEach(function (a) {
+    a.classList.remove("link-preview");
+    a.removeAttribute("data-linkpreview-href");
+  });
+});
