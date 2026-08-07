b3f8bed00b05432b3a15d65c2d29cf6973ecaf30
Tiago Lubiana <tiago.lubiana.alves@usp.br>
2026-08-07 10:25:15 -0300

    Remove custom css for hovering; testing if it works on RTD

--- _static/main.js
+++ _static/main.js
@@ -1,8 +1,3 @@
 $(document).ready( function () {
     $('table.datatable').DataTable();
-
-    document.querySelectorAll('.bd-content .sd-card, .bd-content .sd-card a').forEach(function (el) {
-        el.setAttribute('rel', 'noprefetch');
-        el.setAttribute('data-turbo-prefetch', 'false');
-    });
 } );
